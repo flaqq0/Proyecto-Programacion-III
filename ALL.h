07261c6498a3e7a4cb1e05df27769cd2a104ec1b
flaqq0 <flavia.honores@utec.edu.pe>
2024-07-13 00:45:05 -0500

    lkmjkmk

--- ALL.h
+++ ALL.h
@@ -4,5 +4,12 @@
 #include "logOrRegis/Planes.h"
 #include "cleanData.h"
 
+string fil = "C:/Users/Usuario/Desktop/Utec/2024-1/PrograIII/ProyectoLimpio/peliculas.csv";
+vector<CMovie> movies = loadCsv(fil);
+
+Premium premium("Premium", 44.90,"La mejor","4k(Ultra HD) + HDR", true, 4, 6);
+Standard standard("Estándard",34.90,"Excelente","1080p (Full HD)",false,2,2);
+Basic basic("Básico",24.90,"Buena","720p (HD)", false,1,1);
+
 
 #endif
--- main.cpp
+++ main.cpp
@@ -10,23 +10,6 @@ string read_file(const string& path) {
     return buffer.str();
 }
 int main() {
-    string fil = "C:/Users/Usuario/Desktop/Utec/2024-1/PrograIII/ProyectoLimpio/peliculas.csv";
-    vector<CMovie> movies = loadCsv(fil);
-    /*
-    for (int i = 0; i < 1; i++) {
-        cout << "ID :" << movies[i].imdb_id << endl;
-        cout << "TITTLE :" << movies[i].title << endl;
-        cout << "PLOT SYNOPSIS :" << movies[i].plot_synopsis << endl;
-        cout << "TAGS :" << movies[i].tags << endl;
-        cout << "SPLIT :" << movies[i].split << endl;
-        cout << "SYNOPSIS SOURCE:" << movies[i].synopsis_source << endl;
-        cout << "- - - - - - - - - - - - - - - - - - - - - - - -" << endl;
-    }*/
-
-    Premium premium("Premium", 44.90,"La mejor","4k(Ultra HD) + HDR", true, 4, 6);
-    Standard standard("Estándard",34.90,"Excelente","1080p (Full HD)",false,2,2);
-    Basic basic("Básico",24.90,"Buena","720p (HD)", false,1,1);
-
     httplib::Server svr;
 
     svr.Get("/", [](const httplib::Request&, httplib::Response& res) {
@@ -41,6 +24,33 @@ int main() {
         std::string js = read_file("../web/script.js");
         res.set_content(js, "application/javascript");
     });
+
+    svr.Get("/auth/login", [](const httplib::Request&, httplib::Response& res) {
+        std::string html = read_file("../web/auth/login.html");
+        res.set_content(html, "text/html");
+    });
+    svr.Get("/auth/login.css", [](const httplib::Request&, httplib::Response& res) {
+        std::string css = read_file("../web/auth/login.css");
+        res.set_content(css, "text/css");
+    });
+    svr.Get("/auth/login.js", [](const httplib::Request&, httplib::Response& res) {
+        std::string js = read_file("../web/auth/login.js");
+        res.set_content(js, "application/javascript");
+    });
+
+    svr.Get("/auth/register", [](const httplib::Request&, httplib::Response& res) {
+        std::string html = read_file("../web/auth/register.html");
+        res.set_content(html, "text/html");
+    });
+    svr.Get("/auth/register.css", [](const httplib::Request&, httplib::Response& res) {
+        std::string css = read_file("../web/auth/register.css");
+        res.set_content(css, "text/css");
+    });
+    svr.Get("/auth/register.js", [](const httplib::Request&, httplib::Response& res) {
+        std::string js = read_file("../web/auth/register.js");
+        res.set_content(js, "application/javascript");
+    });
+
 
     svr.listen("localhost", 8080);
     return 0;
